059d8e2b54649ff0b1a2496332e0e202c96624d9
cooligus <pewienjaaa@gmail.com>
2023-02-18 19:32:16 +0100

    Add edit and delete options

--- src/Controller/StudentsController.php
+++ src/Controller/StudentsController.php
@@ -2,23 +2,91 @@
 
 namespace App\Controller;
 
+use App\Entity\Student;
+use App\Form\StudentType;
+use App\Form\StudentUpdateType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StudentsController extends AbstractController
 {
+    /**
+     * @var ObjectManager
+     */
+    private $orm;
+
+    public function __construct(ManagerRegistry $doctrine) 
+    {
+        $this->orm = $doctrine->getManager();
+    }
+
     #[Route('/students', name: 'app_students')]
     public function index(): Response
     {
+        $students = $this->orm->getRepository(Student::class)->findAll();
+        foreach($students as $student) {
+            $student->getSex() 
+                ? $student->setSex('Male')
+                : $student->setSex('Female');
+        }
         return $this->render('students/index.html.twig', [
-            'students' => [['id' => 2, 'name' => 'Jimmy', 'lastname' => 'Week', 'sex' => 'man']],
+            'students' => $students,
         ]);
     }
 
     #[Route('/add-student', name: 'app_addStudent')]
-    public function FunctionName(Type $var = null)
+    public function createStudent(Request $request, ManagerRegistry $doctrine): Response
+    { 
+        $student = new Student();
+        $form = $this->createForm(StudentType::class, $student);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $student = $form->getData();
+
+            $this->orm->persist($student);
+            $this->orm->flush();
+
+            return $this->redirectToRoute('app_students');
+        }
+
+        return $this->render('students/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/delete-student/{id}', name: 'app_deleteStudent')]
+    public function deleteStudent(int $id, ManagerRegistry $doctrine)
     {
-        # code...
+        $student = $this->orm->getRepository(Student::class)->find($id);
+        if($student) {
+            $this->orm->remove($student);
+            $this->orm->flush();
+        }
+        return $this->redirectToRoute('app_students');
+    }
+
+    // It's sus, but sometimes in lastnames can be character '-'.
+    #[Route('/edit-student/{id}', name: 'app_updateStudent', defaults:['name' => null, 'lastname' => null, 'sex' => null])]
+    public function editStudent(int $id, Request $request)
+    {
+        $student = $this->orm->getRepository(Student::class)->find($id);
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $student = $form->getData();
+
+            $this->orm->persist($student);
+            $this->orm->flush();
+
+            return $this->redirectToRoute('app_students');
+        }
+
+        return $this->render('students/new.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
---
+++ src/Entity/Student.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudentRepository::class)]
+class Student
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(type: Types::BOOLEAN)]
+    private $sex = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getSex()
+    {
+        return $this->sex;
+    }
+
+    public function setSex($sex): self
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+
+    public function isSex(): ?bool
+    {
+        return $this->sex;
+    }
+}
